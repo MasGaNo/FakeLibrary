58f825fa7af0899e1b1d0feedd99cf5ab4b3a931
MasGaNo <masgano@gmail.com>
2016-09-30 08:16:21 +0200

    Change module system to UMD

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,20 @@
-"use strict";
-var FakeLibrartClass = (function () {
-    function FakeLibrartClass() {
+(function (factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
     }
-    FakeLibrartClass.prototype.foo = function () {
-        console.log("I'm a Fake Library Class");
-    };
-    return FakeLibrartClass;
-}());
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = FakeLibrartClass;
+    else if (typeof define === 'function' && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    var FakeLibrartClass = (function () {
+        function FakeLibrartClass() {
+        }
+        FakeLibrartClass.prototype.foo = function () {
+            console.log("I'm a Fake Library Class");
+        };
+        return FakeLibrartClass;
+    }());
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = FakeLibrartClass;
+});
